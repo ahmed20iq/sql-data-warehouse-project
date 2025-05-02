39a6a976929717af665f7413a50bde533172c95e
Ahmed Adil <ahadkh16@gmail.com>
2025-05-02 11:52:18 +0300

    modified proc_load file & add ROWTERMINATOR

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -3,14 +3,14 @@
 Stored Procedure: Load Bronze Layer (Source -> Bronze)
 ===============================================================================
 Script Purpose:
-    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    This stored procedure loads data into the 'bronze' schema from external CSV files.
     It performs the following actions:
     - Truncates the bronze tables before loading data.
     - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
 
 Parameters:
-    None. 
-	  This stored procedure does not accept any parameters or return any values.
+    None.
+	This stored procedure does not accept any parameters or return any values.
 
 Usage Example:
     EXEC bronze.load_bronze;
@@ -38,6 +38,7 @@ BEGIN
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
+			ROWTERMINATOR = '\n',
 			TABLOCK
 		);
 		SET @end_time = GETDATE();
